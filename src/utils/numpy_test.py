5ac8a4210555f4d441d022f7a6fdf96b0bbc116e
djology <djology.w@gmail.com>
2022-07-22 19:15:29 +0800

    update

---
+++ src/utils/numpy_test.py
@@ -0,0 +1,18 @@
+'''
+@Author: djology.w
+@Date: 2022/7/13 下午12:14
+@Desc: 
+'''
+
+import numpy as np
+
+# 创建全零的数组
+a = np.zeros((3, 2))
+#  创建全一的数组
+np.ones((2, 4))
+# 创建一个递增或递减的数列
+np.arange(3, 7)
+# 返回介于某个区间
+np.linspace(0, 1, 5)
+
+var = a.shape
